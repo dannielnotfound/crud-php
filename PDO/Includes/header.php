d8655e8b3eddf1801a48f17a27c656b888593438
dannielnotfound <danielriber548@gmail.com>
2022-05-15 21:00:23 -0300

    Criação da pasta Includes e divisão de conteúdo.

---
+++ PDO/Includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>CRUD - Php</title>
+</head>
+<body>
+    <header>
+        <div class="box">
+            <h1>CRUD: <span> PDO </span>&<span> MYSQLi </span></h1> 
+        </div>
+        <nav>
+            <ul>
+                <li><a href="">Cadastro</a></li>
+                <li><a href="">Seleção</a></li>
+                <li><a href="">Deletar</a></li>
+                <li><a href="">Atualização</a></li>
+            </ul>
+        </nav>
+    </header>
--- PDO/index.php
+++ PDO/index.php
@@ -1,32 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>CRUD - Php</title>
-</head>
-<body>
-    <header>
-        <div class="box">
-            <h1>CRUD: <span> PDO </span>&<span> MYSQLi </span></h1> 
-        </div>
-        <nav>
-            <ul>
-                <li><a href="">Cadastro</a></li>
-                <li><a href="">Seleção</a></li>
-                <li><a href="">Deletar</a></li>
-                <li><a href="">Atualização</a></li>
-            </ul>
-        </nav>
-    </header>
-    
+
+<?php include("Includes/header.php"); ?>
     <main>
-    <?php 
-        include("Class/ClassConexao.php");
-    ?>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam blanditiis atque assumenda. Nihil nulla, a eligendi laboriosam, tempore eaque consequatur porro, sapiente dignissimos ullam aliquid ea repellendus corrupti similique quisquam?
+        <?php include("Class/ClassConexao.php"); ?>
     </main>
-</body>
-</html>
+<?php include("Includes/footer.php"); ?>
